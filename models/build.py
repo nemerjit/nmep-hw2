1c67e259e3db98a63a3f4592ec910bc5af5b7f60
Reem Elyas <110420942+ReemElyas@users.noreply.github.com>
2024-04-06 22:47:10 -0700

    Update build.py

--- models/build.py
+++ models/build.py
@@ -1,12 +1,13 @@
 from .lenet import LeNet
 from .resnet import ResNet18
+from alexnet import AlexNet
 
 
 def build_model(config):
     "Model builder."
 
     model_type = config.MODEL.NAME
-
+    
     if model_type == 'lenet':
         model = LeNet(num_classes=config.MODEL.NUM_CLASSES)
     elif model_type == 'resnet18':
@@ -19,6 +20,8 @@ def build_model(config):
     #     model = ResNet101(num_classes=config.MODEL.NUM_CLASSES)
     # elif model_type == 'resnet152':
     #     model = ResNet152(num_classes=config.MODEL.NUM_CLASSES)
+      elif model_type == 'alexnet':  
+          model = AlexNet(num_classes=config.MODEL.NUM_CLASSES)  
     else:
         raise NotImplementedError(f"Unknown model: {model_type}")
     
